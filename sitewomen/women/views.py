1dd61b9083264c5f9a4bea1775741ade86d3bdbc
Vladimir <vlad2aria@mail.ru>
2023-12-04 17:17:57 +0300

    add converters

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -7,5 +7,11 @@ def index(request: HttpRequest):
     return HttpResponse('Страница приложения women')
 
 
-def categories(request: HttpRequest):
-    return HttpResponse('<h1> статьи по категориям </h1>')
+def categories(request: HttpRequest, cat_id):
+    return HttpResponse(f'<h1> статьи по категориям </h1><p> id:{cat_id}</p>')
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1> статьи по категориям </h1><p> slug:{cat_slug}</p>')
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам </h1><p> year :{year}</p>')
