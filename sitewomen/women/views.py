1123e9eead9f5a5319f6f7b4a504da1caf158935
Vladimir Khromov <vlad2aria@mail.ru>
2023-12-06 16:41:59 +0300

    add Redirect

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+
 
 # Create your views here.
 
@@ -11,11 +13,16 @@ def categories(request: HttpRequest, cat_id):
     print(request.GET)
     return HttpResponse(f'<h1> статьи по категориям </h1><p> id:{cat_id}</p>')
 
+
 def categories_by_slug(request, cat_slug):
     print(request.GET)
     return HttpResponse(f'<h1> статьи по категориям </h1><p> slug:{cat_slug}</p>')
 
+
 def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('music',))
+        return HttpResponseRedirect(uri)
     return HttpResponse(f'<h1>Архив по годам </h1><p> year :{year}</p>')
 
 
